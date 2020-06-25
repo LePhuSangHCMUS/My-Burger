c0bc34dbb9394cddad9058e45d61eeafa8f822d2
lexx <lesangg@gmail.com>
2020-06-25 09:47:47 +0700

    new ne

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import classes from './App.module.css';
-import Layout from './components/layouts/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
-import RouterRedirect from './RouterRedirect'
-import { Provider } from 'react-redux';
-import withErrorHandle from '../src/hoc/withErrorHandle/withErrorHandle'
-import store from './store/store'
-function App() {
-  return (
-    <div className={classes.App}>
-      <Provider store={store}>
-        <Router>
-          <Layout>
-            <RouterRedirect />
-          </Layout>
-        </Router>
-      </Provider>
-    </div>
-  );
-}
+// import React from 'react';
+// import classes from './App.module.css';
+// import Layout from './components/layouts/Layout'
+// import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+// import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+// import RouterRedirect from './RouterRedirect'
+// import { Provider } from 'react-redux';
+// import withErrorHandle from '../src/hoc/withErrorHandle/withErrorHandle'
+// import store from './store/store'
+// function App() {
+//   return (
+//     <div className={classes.App}>
+//       <Provider store={store}>
+//         <Router>
+//           <Layout>
+//             <RouterRedirect />
+//           </Layout>
+//         </Router>
+//       </Provider>
+//     </div>
+//   );
+// }
 
-export default withErrorHandle(App);
+// export default withErrorHandle(App);
